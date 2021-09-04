b442dbcd2c6cd4f8b333e359923bbc85ece847ca
VISHNU MATE <matevishnu27@gmail.com>
2021-09-04 18:51:26 +0530

    New changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Center(
+            child: Text('Ask Me Anything'),
+          ),
+        ),
+        body: Askmeanything(),
+      ),
+    ),
+  );
+}
+
+class Askmeanything extends StatefulWidget {
+  const Askmeanything({Key key}) : super(key: key);
+
+  @override
+  _AskmeanythingState createState() => _AskmeanythingState();
+}
+
+class _AskmeanythingState extends State<Askmeanything> {
+  int ballnumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballnumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballnumber.png'),
+        ),
       ),
     );
+  }
+}
